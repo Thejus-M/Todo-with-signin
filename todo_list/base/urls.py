20a3de201515e4392daea255dfe433bd8c6a622d
Thejus <git@tigerraw22.anonaddy.com>
2022-03-28 23:21:08 +0530

    Logout done

--- todo_list/base/urls.py
+++ todo_list/base/urls.py
@@ -3,8 +3,11 @@ from django.urls import path
 from .views import (TaskCreateView, TaskDeleteView, TaskDetail, TaskList,
                     TaskUpdateView,CustomLoginView)
 
+from django.contrib.auth.views import LogoutView
+
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('',TaskList.as_view(),name='tasks'),
     path('task/<int:pk>/',TaskDetail.as_view(),name='task'),
     path('create-task/',TaskCreateView.as_view(),name='create-task'),
