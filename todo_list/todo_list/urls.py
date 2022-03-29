6b4704303a61ace2ac11f009bd8fb233186545cf
Thejus <git@tigerraw22.anonaddy.com>
2022-03-29 17:08:10 +0530

    some changes to add to heroku

--- todo_list/todo_list/urls.py
+++ todo_list/todo_list/urls.py
@@ -14,9 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import include, path
+from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('base.urls')),
+    path('admin/', admin.site.urls),
+    path('', include("Home.urls")),
+
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
